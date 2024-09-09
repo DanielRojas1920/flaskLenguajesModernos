065e4e08f08b5e7dcb4c476e290a70a1eb3a8236
DanielRojas1920 <danielrojvilla@gmail.com>
2024-09-08 19:45:33 -0600

    Update, Delete

--- app.py
+++ app.py
@@ -8,30 +8,109 @@ sys.path.append(path)
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-count = 0
+
 notes = []
-connection = MySQLConnection()
+connection = MySQLConnection(
+    host= "localhost",
+    user="flaskAgenda",
+    password="flaskAgenda2024",
+    database = "flaskagenda"
+)
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html", notes = notes)
 
+@app.route('/DeleteNote', methods = ['POST'])
+def delete_note():
+    global notes
+    index = request.form['noteid']
+
+    connection.connect()
+
+    connection.delete('agendanotes', index)
+
+    notes = []
+    for value in connection.select('agendanotes'):
+        notes.append(value)
+
+    connection.disconnect()
+
+    return redirect('/')
+
+@app.route('/UpdateNotePage', methods=['POST'])
+def update_note_page():
+    global notes
+    index = int(request.form['noteid'])
+    aux_note = None
+    for note in notes:
+        if (note[0] == index):
+
+            aux_note = list(note)
+            break
+
+    print(aux_note)
+
+    return render_template("updateNote.html", note = aux_note)
+
+@app.route('/UpdateNotePage/Update', methods=['POST'])
+def update_note():
+    global notes
+
+    title = request.form['textbox1']
+    date = request.form['fecha']
+    details = request.form['details']
+    id = request.form['noteid']
+    date = '-'.join(date.split('/')[::-1])
+    insert_values = [[title, '%s'], [date, '%s'], [details, '%s']]
+
+    connection.connect()
+    connection.update('agendanotes', id ,insert_values)
+    notes = []
+
+    for value in connection.select('agendanotes'):
+        notes.append(value)
+
+    connection.disconnect()
+
+
+
+    return redirect('/')
+
+
+
+
+
 @app.route('/NewNote', methods=['POST'])
 def new_note():
-    global count
-    value = request.form['textbox1']
-    count = count +1
-    notes.append((count, value))
-    insert_values = [[value, '%s']]
-    connection.insert('notes', insert_values)
+    global notes
+
+    title = request.form['textbox1']
+    date = request.form['fecha']
+    details = request.form['details']
+    date = '-'.join(date.split('/')[::-1])
+    insert_values = [[title, '%s'], [date, '%s'], [details, '%s']]
+
+    connection.connect()
+    connection.insert('agendanotes', insert_values)
+    notes = []
+
+    for value in connection.select('agendanotes'):
+        notes.append(value)
+
+    connection.disconnect()
 
 
     return redirect('/')
 
  
 if __name__ == '__main__':
-    for value in connection.select('notes'):
+    connection.connect()
+
+    for value in connection.select('agendanotes'):
         notes.append(value)
-        count= value[0]
+
+    connection.disconnect()
+    
     app.run()
--- connection.py
+++ connection.py
@@ -1,17 +1,32 @@
 import mysql.connector
 
 tables = {
-    'notes': ['Note']
+    'agendanotes': ['idNotes','note', '_date', 'details']
 }
  
 class MySQLConnection():
-    def __init__(self) -> None:
-        self.mydb = mysql.connector.connect( #Quité los datos de inicio de sesión
-                host="localhost",
-                user="Username",
-                password="password",
-                database = "database"
-                )
+    def __init__(self, host, user, password, database) -> None:
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.mydb = None
+        
+    def connect(self):
+        try:
+            self.mydb = mysql.connector.connect(
+                host = self.host,
+                user = self.user,
+                password = self.password,
+                database = self.database
+            )
+
+        except Exception as err:
+            print('no se estableció la conexión')
+    
+    def disconnect(self):
+        if (self.mydb.is_connected()):
+            self.mydb.close()
         
     def select(self, table):
         query = f"SELECT * FROM {table}"
@@ -27,7 +42,7 @@ class MySQLConnection():
         insert_values_type = ''
 
 
-        for i in tables[table]:
+        for i in tables[table][1:]:
             columns += f'{i}, '
 
         columns = columns[:-2]
@@ -44,4 +59,33 @@ class MySQLConnection():
         cursor.execute(query, insert_values)
 
         self.mydb.commit()
+
+    def delete(self, table, id):
+        query= f'DELETE FROM {table} WHERE {tables[table][0]} = {id}'
+
+        cursor = self.mydb.cursor()
+
+        cursor.execute(query)
+        
+        self.mydb.commit()
+
+    def update(self, table, id, values):
+        str_values = ''
+        aux_table = tables[table]
+        insert_values = []
+
+        for i in range(len(values)):
+            str_values += f'{aux_table[i+1]} = {values[i][1]}, '
+            insert_values.append(values[i][0])
+
+        str_values = str_values[:-2]
+
+        query= f'UPDATE {table} SET {str_values} WHERE {aux_table[0]} = {id}'
+
+        cursor = self.mydb.cursor()
+
+        cursor.execute(query, insert_values)
+
+        self.mydb.commit()
+
 
